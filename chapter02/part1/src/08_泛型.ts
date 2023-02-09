9a9904f597f7305364341d5d659d17bf082e1995
WangNing-Zhou <1879154660@qq.com>
2023-02-10 06:34:48 +0800

    ts环境搭建

---
+++ chapter02/part1/src/08_泛型.ts
@@ -0,0 +1,41 @@
+// function fn(a:any):any{
+//     return a;
+// }
+
+
+
+/**
+ * 在定义函数或是类时,如果遇到类型不明确就可以使用泛型
+ */
+
+function fun<T> (a:T): T{
+    return a
+}
+//可以直接调用具有泛型的函数
+const result = fun(10) //不指定泛型,TS可以自动对类型进行推断
+const result2 = fun<string>('hello') //指定泛型
+
+function  fn2<T,K>(a:T,b:K):T{
+    console.log(b)
+    return a;
+}
+
+fn2<number,string>(123,'hello')
+
+interface  Inter{
+    length: number;
+}
+
+function  fn3<T extends Inter>(a:T):number{
+    return a.length
+}
+
+class MyClass<T> {
+    name: T;
+    constructor(name:T) {
+        this.name = name;
+    }
+}
+
+
+const mc = new MyClass<String>('勋悟空')
