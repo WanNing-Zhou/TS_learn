d34eb96485aea2d503135a28d13f3dfaa54d9d04
WangNing-Zhou <1879154660@qq.com>
2023-02-10 05:04:58 +0800

    ts环境搭建

---
+++ chapter02/part1/dist/02_构造函数.js
@@ -0,0 +1,24 @@
+"use strict";
+class Dog {
+    /**
+     *constructor被称为构造函数
+     * 构造函数会在对象创建时调用
+     */
+    constructor(name, age) {
+        this.name = '旺财';
+        this.age = 3;
+        this.name = name;
+        this.age = age;
+    }
+    bark() {
+        alert('汪汪汪');
+        //在方法中可以通过this来表示当前调用方法的对象
+        console.log(this);
+    }
+}
+const dog1 = new Dog('勋悟空', 2);
+const dog2 = new Dog('往往', 3);
+const dog3 = new Dog('开发', 4);
+const dog4 = new Dog('狗管理', 4);
+console.log(dog1 === dog2);
+dog1.bark();
