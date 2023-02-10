421def06cbd5b4a906e64ddf77b1db6631397350
WangNing-Zhou <1879154660@qq.com>
2023-02-10 22:47:19 +0800

    ts环境搭建

--- chapter02/part2/src/index.ts
+++ chapter02/part2/src/index.ts
@@ -1,12 +1,17 @@
 //引入样式
 import './style/index.less'
+import  Food from './modules/Food'
+import ScorePanel from "./modules/ScorePanel";
 
-//定义食物类Food
-class Food{
-    //定义一个属性表示食物所对应的元素
-    element:HTMLElement;
-    constructor() {
-        //获取页面中的food元素，并将器赋值给element
-        this.element = document.getElementById('food')!;
-    }
-}
+
+
+const food = new Food();
+food.change()
+console.log(food.X,food.Y)
+
+
+
+
+
+
+
---
+++ chapter02/part2/src/modules/Snake.ts
@@ -0,0 +1,7 @@
+export default class Snake{
+    //表示蛇的元素
+    element:HTMLElement;
+    constructor() {
+        this.element = document.getElementById('snake')!
+    }
+}
